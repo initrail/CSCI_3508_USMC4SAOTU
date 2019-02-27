d5161f793485a76c60ff34d9d3216b4ff3049d59
Chanheeshin <chanheeshin@live.com>
2019-02-26 17:38:47 -0700

    Set up data_reader and main code
    -main file grabs command line arguments for height and width
    -main function runs the data_reader file to read test JSON input file
    -data_reader at the moment cannot be run on stdin since PyCharm does not support it, will have to test later.

---
+++ CSCI_3508_USMC4SAOTU/data_reader.py
@@ -0,0 +1,21 @@
+import sys,json
+import numpy as np
+
+class data_reader:
+    def __init__(self, width, height):
+        self.grid = np.zeros((height,), dtype=int)
+        self.width = width
+        self.height = height
+
+    def readGrid(self):
+        with open('test.json') as f:
+            data = json.load(f)
+
+#        data = json.load(sys.stdin)
+        for column in data['grid']:
+            g = np.asarray(column)
+            self.grid = np.column_stack((self.grid, g))
+
+        self.grid = np.delete(self.grid, 0,axis=1)
+
+        return self.grid
--- CSCI_3508_USMC4SAOTU/main.py
+++ CSCI_3508_USMC4SAOTU/main.py
@@ -1,5 +1,23 @@
-def main():
-    print("Please don't touch yet")
+from data_reader import data_reader as dr
+import argparse
 
-if __name__ == "main":
+def main():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('height', type=int, help='an integer for the height of the grid')
+    parser.add_argument('width', type=int, help='an integer for the width of the grid')
+
+    args = parser.parse_args()
+
+    height = args.__dict__["height"]
+    width = args.__dict__["width"]
+
+    print("height = ", flush=True, end="")
+    print(args.__dict__["height"])
+    print("width = ", flush=True, end="")
+    print(args.__dict__["width"])
+
+    newReader = dr(width, height)
+    print(newReader.readGrid())
+
+if __name__ == '__main__':
     main()
